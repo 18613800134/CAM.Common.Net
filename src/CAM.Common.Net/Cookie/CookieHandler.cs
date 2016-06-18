6c941c1a4ea200d0e72297ede2b12f8ff32098f1
iambluebird <6891122@qq.com>
2016-06-18 19:16:12 +0800

    Publish

---
+++ src/CAM.Common.Net/Cookie/CookieHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CAM.Common.Net.Cookie
+{
+    public class CookieHandler
+    {
+        public static void set(string key, string value)
+        {
+            HttpContext.Current.Response.Cookies.Add(new HttpCookie(key, value));
+        }
+
+        public static string read(string key)
+        {
+            string value = "";
+            HttpCookie cookie = HttpContext.Current.Request.Cookies[key];
+            if (cookie != null)
+            {
+                value = cookie.Value;
+            }
+            return value;
+        }
+
+        public static void remove(string key)
+        {
+            HttpCookie cookie = HttpContext.Current.Response.Cookies[key];
+            if (cookie != null)
+            {
+                cookie.Value = null;
+                cookie.Expires = DateTime.Now.AddMonths(-1);
+            }
+        }
+    }
+}
---
+++ src/CAM.Common.Net/Http/HttpHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace CAM.Common.Net.Http
+{
+    public class HttpHandler
+    {
+
+        public static string get(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
+        }
+
+        public static string post(string url, string postData)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = Encoding.UTF8.GetByteCount(postData);
+            Stream myRequestStream = request.GetRequestStream();
+            StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding("gb2312"));
+            myStreamWriter.Write(postData);
+            myStreamWriter.Close();
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
+        }
+
+    }
+}
